411fe21a096be89ce2320bb2b726f03e88879383
Bayisademise <bayisademise@gmail.com>
2025-05-24 17:52:21 +0300

    frist commit

---
+++ lms/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Route, Routes, useMatch } from 'react-router-dom';
+import Home from './Pages/Sudents/Home';
+import CourseList from './Pages/Sudents/CourseList';
+import CourseDetail from './Pages/Sudents/CourseDetail';
+import MyEnrollement from './Pages/Sudents/MyEnrollement';
+import Player from './Pages/Sudents/Player';
+import Educator from './Pages/Educator/Educator';
+import AddCourse from './Pages/Educator/AddCourse';
+import MyCourse from './Pages/Educator/MyCourse';
+import SudentEnrollement from './Pages/Educator/SudentEnrollement';
+import Navbar from './Components/Students/Navbar';
+import Loading from './Components/Students/Loading';
+import Footer from './Components/Students/Footer';
+import "quill/dist/quill.snow.css"
+import Dashboard from './Pages/Educator/Dashboard';
+const App = () => {
+  const isEducatorRoute = useMatch('/educator/*');
+  return (
+    <div className="text-default min-h-screen bg-white">
+      {!isEducatorRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Course-list/:input?" element={<CourseList />} />
+        <Route path="/Course/:input" element={<CourseDetail />} />
+        <Route path="/my-enrollment" element={<MyEnrollement />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path='/load' element ={<Loading />}/>
+        <Route path='/footer' element ={<Footer />}/>
+        <Route path="/educator" element={<Educator />}>
+          <Route path='/educator' element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-course" element={<MyCourse />} />
+          <Route path="student-enrollment" element={<SudentEnrollement />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;

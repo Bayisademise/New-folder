4c357bc829cc553f62426ae37ff95f6901275ec9
Bayisademise <bayisademise@gmail.com>
2025-06-02 10:06:13 +0300

    git added

--- server/server.js
+++ server/server.js
@@ -2,27 +2,24 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
-import { clerkWebhooks } from "./controller/webooks.js";
+import { clerkWebhooks } from "./controller/webhooks.js";
 
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+await connectDB()
+app.use(cors())
 
 // Routes
-app.get("/", (req, res) => res.send("API is working"));
-app.post('/clerk',  express.json(), clerkWebhooks);
+app.get("/", (req, res) => res.send("API is working"))
+app.post('/clerk', express.json(), clerkWebhooks)
 
-// Catch-all route for 404 errors
-app.use((req, res) => {
-  res.status(404).json({ success: false, message: "Route not found" });
-});
+// Start server only if not in Vercel environment
 
-// Connect to MongoDB
-connectDB().catch((err) => console.error("MongoDB connection error:", err));
+  const PORT = process.env.PORT || 5000
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+export default app;
